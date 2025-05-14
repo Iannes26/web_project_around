1ea70dbf1e132f590827f9ea66e3a9a4b9361b35
Erick Iannes <erick.iannes26@gmail.com>
2025-05-14 20:07:13 +0200

    nova atualizacao do projeto

---
+++ script/script.js
@@ -0,0 +1,32 @@
+let profile = document.querySelector(".profile");
+let editButton = document.querySelector(".profile__button-edit");
+let popup = document.querySelector(".popup");
+let closeButton = document.querySelector(".popup__button");
+let profileName = document.querySelector(".profile__name");
+let inputName = document.querySelector(".form__item-name");
+let inputInfo = document.querySelector(".form__item-info");
+let profileInfo = document.querySelector(".profile__info");
+let formElement = document.querySelector(".form");
+let formButton = document.querySelector(".form__button");
+
+function openPopup() {
+  popup.className = "popup_opened";
+  inputName.value = profileName.textContent;
+  inputInfo.value = profileInfo.textContent;
+}
+
+editButton.addEventListener("click", openPopup);
+
+function closePopup() {
+  popup.className = "popup";
+}
+
+closeButton.addEventListener("click", closePopup);
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileInfo.textContent = inputInfo.value;
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
